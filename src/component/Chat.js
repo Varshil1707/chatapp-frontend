72a0e51038d15bc052a6d468df6b3688a2041c5b
Varshil <varshil.shah@ecosmob.com>
2022-12-22 16:45:02 +0530

    Test

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import socketIo from "socket.io-client";
 import "./Chat.css";
@@ -6,23 +5,24 @@ import Message from "./Message";
 
 const ENDPOINT = "https://chatapp-backend-ne38.onrender.com";
 // const ENDPOINT = "http://localhost:6001/"
+let socket
 
 const Chat = () => {
-  const socket = socketIo(ENDPOINT, { transports: ["websocket"] });
   const user = localStorage.getItem("user");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [id, setId] = useState("");
-
+  
   const send = () => {
     // const message = document.getElementById('chatInput').value
     socket.emit("message", { message, id });
-    console.log(id)
+    console.log(id);
     setMessage("");
   };
   useEffect(() => {
+    socket = socketIo(ENDPOINT, { transports: ["websocket"] });
+    console.log(socket)
     socket.on("connect", () => {
-
       console.log("connected");
       setId(socket.id);
     });
@@ -33,46 +33,41 @@ const Chat = () => {
     socket.on("userJoined", (data) => {
       setMessages([...messages, data]);
     });
-   
-    socket.on("leave", (data) => {
-      setMessages([...messages, data]);
-    });
-
-
 
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     socket.on("sentMessage", (data) => {
       setMessages([...messages, data]);
-      console.log(`${data.user} : ${data.message}` );
+      console.log(`${data.user} : ${data.message}`);
     });
-    
-  },[messages])
-
+  }, [messages]);
 
   return (
     <div className="chatPage">
       <div className="chatContainer">
         <div className="header">
-        <h2>Chat App</h2>
+          <h2>Chat App</h2>
         </div>
         <div className="chatBox">
           {messages.map((data, i) => (
-            <Message message={data.message} classes={data.id === id ? 'Right' : 'Left'} user={data.id === id ? "" : data.user} />
+            <Message
+              message={data.message}
+              classes={data.id === id ? "Right" : "Left"}
+              user={data.id === id ? "" : data.user}
+            />
           ))}
         </div>
         <div className="inputBox">
           <input
-          placeholder="Enter Message Here"
+            placeholder="Enter Message Here"
             className="inputBoxInput"
             id="chatInput"
             onChange={(e) => setMessage(e.target.value)}
             type="text"
             value={message}
-            
           />
-          <button className="inputBoxButton" onClick={send} >
+          <button className="inputBoxButton" onClick={send}>
             Send
           </button>
         </div>
