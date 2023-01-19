b5c5e3f879f02ecd8fa67d1f9685f6fd6ca303f7
Varshil <varshil.shah@ecosmob.com>
2023-01-19 12:47:47 +0530

    Jan 19

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -5,8 +5,8 @@ import Message from "./Message";
 import SendIcon from "@mui/icons-material/Send";
 import Sidebar from "./Sidebar";
 
-// const ENDPOINT = "https://chatapp-backend-ne38.onrender.com";
-const ENDPOINT = "http://localhost:6002/";
+const ENDPOINT = "https://chatapp-backend-ne38.onrender.com";
+// const ENDPOINT = "http://localhost:6002/";
 let socket;
 
 const Chat = () => {
